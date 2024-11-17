936c54700aa54816b6b867fe56bf7e69309f121f
Prakhar Verma <156530259+VirtualPrakhar@users.noreply.github.com>
2024-11-17 08:17:58 +0000

    some added

---
+++ realtime-editor/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="homePageWrapper">
+        <div className="formWrapper">
+            <img src="/code-sync.png" alt="code-sync-logo" />
+        </div>
+    </div>
+  )
+}
+
+export default Home
