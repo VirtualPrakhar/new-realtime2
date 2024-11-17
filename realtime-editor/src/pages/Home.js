f1c98a0d54129a36d2c4dcd87876a4c0bd285737
Prakhar Verma <156530259+VirtualPrakhar@users.noreply.github.com>
2024-11-17 08:40:41 +0000

    new added

--- realtime-editor/src/pages/Home.js
+++ realtime-editor/src/pages/Home.js
@@ -5,7 +5,31 @@ const Home = () => {
     <div className="homePageWrapper">
         <div className="formWrapper">
             <img src="/code-sync.png" alt="code-sync-logo" />
+            <h4 className="mainlabel">Paste Invitation Room ID</h4>
+            <div className="inputGroup">
+              <input 
+                type="text" 
+                className="inputBox" 
+                placeholder="ROOM ID"
+              />
+              <input 
+                type="text" 
+                className="inputBox" 
+                placeholder="USERNAME"
+              />
+              <button className="btn joinBtn">Join</button>
+              <span className="createInfo">
+                If you don't have an Invite then create &nbsp;
+                <a href="" className="createNewBtn">
+                  New Room
+                </a>
+              </span>
+            </div>
         </div>
+        <footer>
+          <h4>Built with &#128155;&nbsp;by{''}
+             <a href="https://www.linkedin.com/in/prakharvermavirtualprakhar/">&nbsp;Virtual Prakhar</a></h4>
+        </footer>
     </div>
   )
 }
